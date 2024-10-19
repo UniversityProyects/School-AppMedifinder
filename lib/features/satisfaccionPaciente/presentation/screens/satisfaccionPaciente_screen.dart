91452aadbd0e750114d08cbcb6886856e6498d9d
IDGS-803-20001430 <lrerick20001430@gmail.com>
2024-10-18 21:57:15 -0600

    Pantalla con conexión a Back para comentarios de medicos

--- lib/features/satisfaccionPaciente/presentation/screens/satisfaccionPaciente_screen.dart
+++ lib/features/satisfaccionPaciente/presentation/screens/satisfaccionPaciente_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:medifinder_crm/config/router/app_router.dart';
 import 'package:medifinder_crm/features/satisfaccionPaciente/presentation/providers/providers.dart';
 import 'package:medifinder_crm/features/shared/shared.dart';
+import 'package:medifinder_crm/features/satisfaccionPaciente/presentation/widgets/widgets.dart';
 
 class SatisfaccionpacienteScreen extends StatelessWidget {
   const SatisfaccionpacienteScreen({super.key});
@@ -16,11 +19,6 @@ class SatisfaccionpacienteScreen extends StatelessWidget {
         title: const Text('Satisfacción Paciente'),
       ),
       body: const _SatisfaccionPacienteView(),
-      floatingActionButton: FloatingActionButton.extended(
-        label: const Text('Nuevo producto'),
-        icon: const Icon(Icons.add),
-        onPressed: () {},
-      ),
     );
   }
 }
@@ -53,10 +51,6 @@ class _SatisfaccionPacienteViewState extends ConsumerState {
       return const Center(child: CircularProgressIndicator());
     }
 
-    if (satisfaccionPacienteState.calificacionesMedico.isEmpty) {
-      return const Center(child: Text('No hay calificaciones disponibles.'));
-    }
-
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemCount: satisfaccionPacienteState.calificacionesMedico.length,
@@ -64,74 +58,13 @@ class _SatisfaccionPacienteViewState extends ConsumerState {
         final calificacion =
             satisfaccionPacienteState.calificacionesMedico[index];
 
-        return Card(
-          elevation: 4,
-          margin: const EdgeInsets.only(bottom: 10.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Icon(Icons.medical_services,
-                    color: Theme.of(context).primaryColor,
-                    size: 40), // Ícono de médico con color primario
-                const SizedBox(width: 16), // Espacio entre el ícono y el texto
-                Expanded(
-                  // Se usa Expanded para que el contenido ocupe el resto del espacio
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${calificacion.nombre} ${calificacion.apellido}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      _buildEstrellas(calificacion.promedioPuntuacion),
-                      const SizedBox(height: 4),
-                      Text(
-                        calificacion.promedioPuntuacion > 0
-                            ? '${calificacion.promedioPuntuacion}'
-                            : 'Sin calificación',
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        calificacion.cantidadComentarios > 0
-                            ? 'Comentarios: ${calificacion.cantidadComentarios}'
-                            : 'No hay comentarios',
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+        return GestureDetector(
+          onTap: () => context.push('/comentarios-medico/${calificacion.id}'),
+          child: CalificacionMedicoCard(
+            calificacionMedico: calificacion,
           ),
         );
       },
-    );
-  }
-
-  // Método para construir las estrellas según la calificación
-  Widget _buildEstrellas(double calificacion) {
-    int estrellasLlenas = calificacion.floor();
-    bool tieneMediaEstrella = (calificacion - estrellasLlenas) >= 0.5;
-
-    return Row(
-      children: List.generate(5, (index) {
-        if (index < estrellasLlenas) {
-          return const Icon(Icons.star, color: Colors.amber);
-        } else if (index == estrellasLlenas && tieneMediaEstrella) {
-          return const Icon(Icons.star_half, color: Colors.amber);
-        } else {
-          return const Icon(Icons.star_border, color: Colors.amber);
-        }
-      }),
     );
   }
 }
