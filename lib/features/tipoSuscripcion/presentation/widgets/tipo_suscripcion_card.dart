7ae0640b57f9a4cbb73dc1e7f2875d234da46b7d
Daniel Salazar <daniel.sabri234@gmail.com>
2024-11-21 23:18:57 -0600

    Validaciones en formularios de tipos de suscripcion

--- lib/features/tipoSuscripcion/presentation/widgets/tipo_suscripcion_card.dart
+++ lib/features/tipoSuscripcion/presentation/widgets/tipo_suscripcion_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:medifinder_crm/features/tipoSuscripcion/domain/domain.dart';
 import 'package:medifinder_crm/features/tipoSuscripcion/infrastructure/mappers/tipoSuscripcionDTO.dart';
 
@@ -19,38 +20,22 @@ class TipoSuscripcionCard extends StatefulWidget {
 }
 
 class _TipoSuscripcionCardState extends State<TipoSuscripcionCard> {
-  late TextEditingController _nombreController;
-  late TextEditingController _descripcionController;
-  late TextEditingController _precioController;
-  late TextEditingController _duracionController;
-
-  late TipoSuscripcion tipoSuscripcion; // Declara la variable aquí
+  late TipoSuscripcion tipoSuscripcion;
 
   @override
   void initState() {
     super.initState();
-    tipoSuscripcion = widget.tipoSuscripcion; // Asigna el valor aquí
-    _nombreController = TextEditingController(text: tipoSuscripcion.nombre);
-    _descripcionController =
-        TextEditingController(text: tipoSuscripcion.descripcion);
-    _precioController =
-        TextEditingController(text: tipoSuscripcion.precio.toString());
-    _duracionController =
-        TextEditingController(text: tipoSuscripcion.duracion.toString());
+    tipoSuscripcion = widget.tipoSuscripcion;
   }
 
   @override
   void dispose() {
-    _nombreController.dispose();
-    _descripcionController.dispose();
-    _precioController.dispose();
-    _duracionController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool esActivo = tipoSuscripcion.estatus == 'Activo'; // Usa la variable aquí
+    bool esActivo = tipoSuscripcion.estatus == 'Activo';
 
     return Card(
       elevation: 4,
@@ -66,7 +51,6 @@ class _TipoSuscripcionCardState extends State<TipoSuscripcionCard> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // Nombre de la suscripción
                 Text(
                   tipoSuscripcion.nombre,
                   style: TextStyle(
@@ -76,7 +60,6 @@ class _TipoSuscripcionCardState extends State<TipoSuscripcionCard> {
                   ),
                 ),
                 const SizedBox(height: 8),
-                // Estatus Actual
                 Text(
                   tipoSuscripcion.estatus,
                   style: TextStyle(
@@ -161,72 +144,145 @@ class _TipoSuscripcionCardState extends State<TipoSuscripcionCard> {
   }
 
   void _showModifyModal(BuildContext context) {
+    final _nombreController =
+        TextEditingController(text: tipoSuscripcion.nombre);
+    final _descripcionController =
+        TextEditingController(text: tipoSuscripcion.descripcion);
+    final _precioController =
+        TextEditingController(text: tipoSuscripcion.precio.toString());
+    final _duracionController =
+        TextEditingController(text: tipoSuscripcion.duracion.toString());
+
+    String? errorMessage;
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Modificar Suscripción'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Nombre'),
-                  controller: _nombreController,
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: const Text('Modificar Suscripción'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    if (errorMessage != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 0),
+                        child: Text(
+                          errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Nombre'),
+                      controller: _nombreController,
+                      maxLength: 20,
+                    ),
+                    TextField(
+                      decoration:
+                          const InputDecoration(labelText: 'Descripción'),
+                      controller: _descripcionController,
+                      maxLength: 20,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Precio'),
+                      controller: _precioController,
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      inputFormatters: [
+                        DecimalInputFormatter(),
+                      ],
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Duración'),
+                      keyboardType: TextInputType.number,
+                      controller: _duracionController,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(3),
+                      ],
+                    ),
+                  ],
                 ),
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Descripción'),
-                  controller: _descripcionController,
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    final nombre = _nombreController.text.trim();
+                    final descripcion = _descripcionController.text.trim();
+                    final precio = double.tryParse(_precioController.text);
+                    final duracion = int.tryParse(_duracionController.text);
+
+                    // Validar campos
+                    if (nombre.isEmpty ||
+                        descripcion.isEmpty ||
+                        precio == null ||
+                        duracion == null) {
+                      setState(() {
+                        errorMessage = 'Todos los campos son obligatorios.';
+                      });
+                      return;
+                    }
+
+                    if (precio <= 0) {
+                      setState(() {
+                        errorMessage = 'El precio tiene que ser mayor a 0.';
+                      });
+                      return;
+                    }
+
+                    if (!RegExp(r'^\d{1,8}(\.\d{1,2})?$')
+                        .hasMatch(precio.toString())) {
+                      setState(() {
+                        errorMessage =
+                            'El precio debe ser un número válido con máximo 8 dígitos y 2 decimales.';
+                      });
+                      return;
+                    }
+
+                    if (duracion <= 0) {
+                      setState(() {
+                        errorMessage = 'La duración tiene que ser mayor a 0.';
+                      });
+                      return;
+                    }
+
+                    final tipoSuscripcionDTO = TipoSuscripcionDTO(
+                      nombre: _nombreController.text,
+                      descripcion: _descripcionController.text,
+                      precio: double.tryParse(_precioController.text) ?? 0.0,
+                      duracion: int.tryParse(_duracionController.text) ?? 0,
+                    );
+
+                    widget.onModify(tipoSuscripcion.id, tipoSuscripcionDTO);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Guardar'),
                 ),
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Precio'),
-                  keyboardType: TextInputType.number,
-                  controller: _precioController,
-                ),
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Duración'),
-                  keyboardType: TextInputType.number,
-                  controller: _duracionController,
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancelar'),
                 ),
               ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                // Validación de campos
-                if (_nombreController.text.isEmpty ||
-                    _descripcionController.text.isEmpty ||
-                    _precioController.text.isEmpty ||
-                    _duracionController.text.isEmpty) {
-                  // Muestra un mensaje de error
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Todos los campos son obligatorios')),
-                  );
-                  return;
-                }
-
-                final tipoSuscripcionDTO = TipoSuscripcionDTO(
-                  nombre: _nombreController.text,
-                  descripcion: _descripcionController.text,
-                  precio: double.tryParse(_precioController.text) ?? 0.0,
-                  duracion: int.tryParse(_duracionController.text) ?? 0,
-                );
-
-                widget.onModify(tipoSuscripcion.id, tipoSuscripcionDTO);
-                Navigator.pop(context);
-              },
-              child: const Text('Guardar'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancelar'),
-            ),
-          ],
+            );
+          },
         );
       },
     );
+  }
+}
+
+class DecimalInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    final regex = RegExp(r'^\d{0,8}(\.\d{0,2})?$');
+
+    if (regex.hasMatch(newValue.text)) {
+      return newValue;
+    }
+
+    return oldValue;
   }
 }
